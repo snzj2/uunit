616e2a12e01b2b19049f605e459f828fd265de9a
Ruslan <baimuhametuv@gmail.com>
2025-04-30 01:35:56 +0300

    на сегодня все

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def index():
     level = request.args.get('level', default=1, type=int)
     return render_template('map.html', active_level=level)
 
+@app.route('/info')
+def info():
+    level = request.args.get('level', default=1, type=int)
+    info = request.args.get('info', default=1, type=str)
+    return render_template('place.html', active_level=level, info_place=info)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
